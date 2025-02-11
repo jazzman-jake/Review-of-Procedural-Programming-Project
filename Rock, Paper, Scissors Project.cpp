cd54d7cba2cd9b921759fcc1d9c3511e5ee86d28
Jake Williamson <williamsonj0403@gmail.com>
2025-02-10 20:57:25 -0500

    Add project files.

---
+++ Rock, Paper, Scissors Project.cpp
@@ -0,0 +1,17 @@
+// Terrence Williamson
+//CSC 222
+//02.10.25
+// Review of Procedural Programming Project
+//R-P-S-Project
+
+#include <iostream>
+#include <random>
+using namespace std;
+
+void scores(), playing();
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
